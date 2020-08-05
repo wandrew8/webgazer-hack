132c39b24953d57d681ae3318f52ed2ec1f056dc
Andrew Weiss <wandrew8@gmail.com>
2020-08-05 10:02:31 -0700

    files initialized and connected with webgazer

---
+++ public/javaScript/app.js
@@ -0,0 +1,18 @@
+webgazer.setGazeListener(function(data, elapsedTime) {
+    if (data == null) {
+        return;
+    }
+    var xprediction = data.x; //these x coordinates are relative to the viewport
+    var yprediction = data.y; //these y coordinates are relative to the viewport
+    console.log(elapsedTime); //elapsed time is based on time since begin was called
+}).begin();
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instance = M.Modal.getInstance(elem);
+    instance.open();
+
+  });
+
+  
+
---
+++ public/javaScript/materialize.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const modal = document.querySelector('.modal');
+    const instance = M.Modal.init(modal, {});
+    instance.open();
+});
+
+
+
